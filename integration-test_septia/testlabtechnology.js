a1c22e3f80c43aadd130b59674a189e8b3c7c47b
SeptiaAmandaAulia <septiaamandaaulia01@gmail.com>
2025-10-16 10:43:17 +0700

    memperbaharui berita

--- integration-test_septia/testlabtechnology.js
+++ integration-test_septia/testlabtechnology.js
@@ -3,28 +3,20 @@ pm.test("Status code is 200", function () {
     pm.response.to.have.status(200);
 });
 
-// Test 2: Memeriksa Properti Wajib pada Setiap Artikel
-pm.test("Each article has required fields (title, description, author)", function () {
-    // Memastikan respons adalah JSON dan menyimpannya
+// Test 2: Memeriksa Properti Wajib pada Setiap Artikel Teknologi
+pm.test("Each technology news article has required fields (title, description, author)", function () {
     const jsonData = pm.response.json();
 
-    // Memastikan array 'articles' ada dan merupakan array
+    // Pastikan properti 'articles' ada
     pm.expect(jsonData).to.have.property('articles');
     pm.expect(jsonData.articles).to.be.an('array');
-    
-    // Melakukan iterasi pada setiap artikel
+
     jsonData.articles.forEach(article => {
-        
-        // 1. Memeriksa Judul (title)
+        // Cek Title
         pm.expect(article, 'Article must have a title').to.have.property("title");
-        
-        // 2. Memeriksa Deskripsi (description)
+        // Cek Description
         pm.expect(article, 'Article must have a description').to.have.property("description");
-        
-        // 3. Memeriksa Penulis (author)
-        // Note: Saya memasukkan 'author' sesuai permintaan tugas sebelumnya,
-        // meskipun kode awal Anda menggunakan 'url'. Anda bisa ganti 'author'
-        // menjadi 'url' jika itu yang Anda inginkan.
+        // Cek Author
         pm.expect(article, 'Article must have an author').to.have.property("author");
     });
 });
